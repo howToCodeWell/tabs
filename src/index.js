fd58aa4d44cad8c2890e23daeb0c265bb8c7da23
peterfisher <c3T-y7J-vo3-qpg>
2020-12-21 23:01:07 +0000

    Adding event and tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Tab from './Components/Tab'
+import TabList from './Components/TabList'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Tab/>
+    <TabList containerClassName={`tab-container`} listClassName={`list-reset flex border-b`}>
+      <Tab label={`First Tab`} className={`bg-gray-500 border-r-2 border-gray-100 inline-block py-2 px-4 text-pink-300 font-semibold hover:text-black hover:bg-pink-300`} onClick={ () => alert('Hello Coders')  } />
+      <Tab label={`Second Tab`} className={`bg-gray-500 inline-block py-2 px-4 text-pink-300 font-semibold hover:text-black hover:bg-pink-300`} onClick={ () => alert('Happy Coding')  } />
+    </TabList>
   </React.StrictMode>,
   document.getElementById('root')
 )
